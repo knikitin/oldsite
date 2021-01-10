c2cd40e8554c6edf5c15a474185376b0dfbdebce
Konstantin Nikitin <kn55@mail.ru>
2021-01-10 23:17:06 +0300

    all from parts

--- new_verbusines/js/script.js
+++ new_verbusines/js/script.js
@@ -1,5 +1,13 @@
-$.get( "parts/menu.html").done(function( data ) {
-    $('body').append( data );
-    alert("sss");
+$.get( "parts/header.html").done(function( data ) {
+    $('.pbheader').append( data );
 });
+
+$.get( "parts/menu.html").done(function( data ) {
+    $('.pbmenu').append( data );
+});
+
+$.get( "parts/footer.html").done(function( data ) {
+    $('.pbfooter').append( data );
+});
+
 
