f478739d5f73e118439601e311c865fc1e384367
Konstantin Nikitin <kn55@mail.ru>
2021-01-13 23:10:51 +0300

    new ver

--- js/script.js
+++ js/script.js
@@ -1,20 +1,23 @@
-// <![CDATA[
-$(function() {
+$.get( "parts/header.html").done(function( data ) {
+    $('.pbheader').append( data );
+});
 
-  // Slider
-  $('#coin-slider').coinslider({width:960,height:360,opacity:1});
+$.get( "parts/menu.html").done(function( data ) {
+    $('.pbmenu').append( data );
 
-  // Radius Box
-  //$('.content .mainbar img.fl, .content .sidebar .gadget, .fbg_resize').css({"border-radius":"8px", "-moz-border-radius":"8px", "-webkit-border-radius":"8px"});
-  //$('.fbg_resize2').css({"border-radius":"9px", "-moz-border-radius":"9px", "-webkit-border-radius":"9px"});
-  //$('.content p.pages span, .content p.pages a').css({"border-radius":"16px", "-moz-border-radius":"16px", "-webkit-border-radius":"16px"});
-  //$('.menu_nav').css({"border-bottom-left-radius":"16px", "border-bottom-right-radius":"16px", "-moz-border-radius-bottomleft":"16px", "-moz-border-radius-bottomright":"16px", "-webkit-border-bottom-left-radius":"16px", "-webkit-border-bottom-right-radius":"16px"});
+    // data = '<span class="sr-only">(текущая)</span>'
+    // $('.pbheader').append( data );
+    var pageType = $('meta[name=description]').attr('content');
+    var menuId = "";
+    if (pageType == "Main") menuId = "#menuMain";
+    if (pageType == "Documents") menuId = "#menuDocuments";
+    if (pageType == "Contacts") menuId = "#menuContacts";
+    if (pageType == "Timetable") menuId = "#menuTimetable";
+    if (menuId != "") $( menuId ).addClass( "active" );    
+});
 
-});	
+$.get( "parts/footer.html").done(function( data ) {
+    $('.pbfooter').append( data );
+});
 
-// Cufon
-Cufon.replace('h1, h2, h3, h4, h5, h6', { hover: true });
-//Cufon.replace('h1', { color: '-linear-gradient(#fff, #ffaf02)'});
-//Cufon.replace('h1 small', { color: '#8a98a5'});
 
-// ]]>
